d498f8b52d4689c2e1c326198c460b95b4fa46aa
San624461 <sanjeda6244611@gmail.com>
2023-09-13 17:31:23 -0400

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Blogs from './Components/Blogs/Blogs'
+import BookMarks from './Components/BookMarks/BookMarks'
 function App() {
 
   return (
     <>
 
       <Header></Header>
+      <main className='md:flex'>
       <Blogs></Blogs>
+      <BookMarks></BookMarks>
+      </main>
 
     </>
   )
---
+++ src/Components/BookMarks/BookMarks.jsx
@@ -0,0 +1,11 @@
+
+
+const BookMarks = () => {
+    return (
+        <div className="md:x-1/3">
+            <h2>BookMarks</h2>
+        </div>
+    );
+};
+
+export default BookMarks;
